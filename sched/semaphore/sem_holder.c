6896110028f05d07f81bd5c29f8ad99b6c63fd9b
David Sidrane <David.Sidrane@NscDg.com>
2022-05-17 15:28:04 -0700

    [DIVERGED] sem_holder:Bug fix:Not restoring base priority
    master:
       With CONFIG_PRIORITY_INHERITANCE set
    
       Three tasks (name:priority)  A:200, B:210, C:220
       A takes semaphore.                A:200
         B waits semaphore.              A:210
           C waits semaphore             A:220
                A posts semaphore        A:210 <- it is removed as a holder and
                  C takes semaphore      A:210    not resotred.
                  C posts semaphore      A:210
                     B takes semaphore   A:210
                     B posts semaphore   A:210
    
       The solution is to look if the thread holds more then the
       current semaphore S1 being posted. Then if the S1 holder is null
       (holder array) or the holder has no more counts (linked list)
       the posting thread's priority is restored to is base priority
       and the pend_reprios is rest.
    
       If the thread does hold another semaphore S2, the the pend_reprios
       highest prority is used. When S2 is posted by the thread it will
       be restorted to it's base priority.
    
    sem_holder:TCB needs to track number of sems held
    
       Priority restoration requiers to keep track of the
       number of unique semaphores that are held by a task.
       This will be used to determine if the removal
       of the holder should revert to the base priority or
       from the pend_reprios array.

--- sched/semaphore/sem_holder.c
+++ sched/semaphore/sem_holder.c
@@ -490,11 +490,12 @@ static int nxsem_restoreholderprio(FAR struct tcb_s *htcb,
    * Perhaps its plan is to kill a thread, then destroy the semaphore.
    */
 
+  pholder = nxsem_findholder(sem, htcb);
+
   if (!nxsched_verify_tcb(htcb))
     {
       swarn("WARNING: TCB %p is a stale handle, counts lost\n", htcb);
 
-      pholder = nxsem_findholder(sem, htcb);
       if (pholder != NULL)
         {
           nxsem_freeholder(sem, pholder);
@@ -508,11 +509,29 @@ static int nxsem_restoreholderprio(FAR struct tcb_s *htcb,
   else if (htcb->sched_priority != htcb->base_priority)
     {
 #if CONFIG_SEM_NNESTPRIO > 0
+      if (htcb->nsem_held == 0 && (pholder == NULL || pholder->counts == 0))
+        {
+          /* if the holder does hold any other semaphores and the running
+           * thread is no longer a holder on this semaphre it can return to
+           * it's base priority .
+           *
+           * In the case of CONFIG_SEM_PREALLOCHOLDERS = 0 It is no longer a
+           * holder if it is not in the holders array.
+           *
+           * In the case of CONFIG_SEM_PREALLOCHOLDERS > 0 it is no longer
+           * a holder if it's count is 0.
+           */
+
+          htcb->npend_reprio = 0;
+        }
+
       /* Are there other, pending priority levels to revert to? */
 
       if (htcb->npend_reprio < 1)
         {
           /* No... the holder thread has only been boosted once.
+           * OR it is no longer holding a count.
+           *
            * npend_reprio should be 0 and the boosted priority should be the
            * priority of the task that just got the semaphore
            * (stcb->sched_priority)
@@ -959,6 +978,16 @@ void nxsem_add_holder_tcb(FAR struct tcb_s *htcb, FAR sem_t *sem)
            */
 
           pholder->htcb = htcb;
+#if CONFIG_SEM_NNESTPRIO > 0
+          if (pholder->counts == 0)
+            {
+              /* This thread/task now holds a count on this semaphore
+               * so add it to the total held.
+               */
+
+              htcb->nsem_held++;
+            }
+#endif
           pholder->counts++;
         }
     }
@@ -1046,6 +1075,17 @@ void nxsem_release_holder(FAR sem_t *sem)
        */
 
       pholder->counts--;
+#if CONFIG_SEM_NNESTPRIO > 0
+      if ((sem->flags & PRIOINHERIT_FLAGS_DISABLE) == 0 &&
+           pholder->counts == 0)
+        {
+          /* This thread/task holds no counts on this semaphore
+           * So remove it from the total held
+           */
+
+          rtcb->nsem_held--;
+        }
+#endif
     }
 }
 
