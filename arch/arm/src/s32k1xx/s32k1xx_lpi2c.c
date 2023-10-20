77a52b09f70c5aa0da904ffee8af14c2bbdad262
David Sidrane <David.Sidrane@NscDg.com>
2023-10-20 09:37:10 -0700

    s32k1xx:lpi2c Timeouts can not be 0

--- arch/arm/src/s32k1xx/s32k1xx_lpi2c.c
+++ arch/arm/src/s32k1xx/s32k1xx_lpi2c.c
@@ -472,6 +472,7 @@ static uint32_t s32k1xx_lpi2c_toticks(int msgc, struct i2c_msg_s *msgs)
 {
   size_t bytecount = 0;
   int i;
+  uint32_t tick = 0;
 
   /* Count the number of bytes left to process */
 
@@ -481,10 +482,16 @@ static uint32_t s32k1xx_lpi2c_toticks(int msgc, struct i2c_msg_s *msgs)
     }
 
   /* Then return a number of microseconds based on a user provided scaling
-   * factor.
+   * factor. It must not be 0.
    */
 
-  return USEC2TICK(CONFIG_S32K1XX_LPI2C_DYNTIMEO_USECPERBYTE * bytecount);
+  tick = USEC2TICK(CONFIG_S32K1XX_LPI2C_DYNTIMEO_USECPERBYTE * bytecount);
+  if (tick == 0)
+    {
+      tick = 1;
+    }
+
+  return tick;
 }
 #endif
 
