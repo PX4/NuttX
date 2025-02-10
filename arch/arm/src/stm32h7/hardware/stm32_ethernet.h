5a786dd8d062b2c66e0829e49608a8bc71d586f6
Julian Oes <julian@oes.ch>
2025-02-11 11:13:08 +1300

    [BACKPORT] stm32h7: enable Ethernet for STM32H7X7XX

--- arch/arm/src/stm32h7/hardware/stm32_ethernet.h
+++ arch/arm/src/stm32h7/hardware/stm32_ethernet.h
@@ -31,7 +31,8 @@
  * families
  */
 
-#if defined(CONFIG_STM32H7_STM32H7X3XX)
+#if defined(CONFIG_STM32H7_STM32H7X3XX) || \
+    defined(CONFIG_STM32H7_STM32H7X7XX)
 
 /****************************************************************************
  * Pre-processor Definitions
