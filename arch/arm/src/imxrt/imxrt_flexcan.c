0b1d47b9219b63395025d47d8cb6b25f7d43c70e
Takashi Furuya <takashi.furuya@nttedt.co.jp>
2026-08-03 15:05:49 +0900

    [BACKPORT] imxrt/flexcan: Initialize union and allow BRS control
    - Zero initialize union in imxrt_transmit()
    - Allow setting BRS from
    
    Signed-off-by: Takashi Furuya <takashi.furuya@nttedt.co.jp>

--- arch/arm/src/imxrt/imxrt_flexcan.c
+++ arch/arm/src/imxrt/imxrt_flexcan.c
@@ -673,6 +673,7 @@ static int imxrt_transmit(struct imxrt_driver_s *priv)
     (peak_tx_mailbox_index_ > mbi ? peak_tx_mailbox_index_ : mbi);
 
   union cs_e cs;
+  cs.cs = 0;
   cs.code = CAN_TXMB_DATAORREMOTE;
   struct mb_s *mb = flexcan_get_mb(priv, mbi);
   mb->cs.code = CAN_TXMB_INACTIVE;
@@ -715,6 +716,7 @@ static int imxrt_transmit(struct imxrt_driver_s *priv)
         }
 
       cs.rtr = frame->can_id & FLAGRTR ? 1 : 0;
+      cs.brs = frame->flags & CANFD_BRS ? 1 : 0;
 
       cs.dlc = g_len_to_can_dlc[frame->len];
 
